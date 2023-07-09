c44473394837df402008f055896d0e650ecff8f6
ClaudZen <claudio.anticoi@outlook.es>
2023-07-09 16:39:04 -0400

    # Feature
    - terraform de creación de recurso de grupos y
    cluster

---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "ic"
+  location = "brazilsouth"
+}
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = "core"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "core"
+
+  default_node_pool {
+    name       = "corepool"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Develop"
+    
+  }
+}
