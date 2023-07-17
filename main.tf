6650ac3a889f8aa6da421323856ba4e0138a8b25
ClaudZen <claudio.anticoi@outlook.es>
2023-07-16 20:24:28 -0400

    fix variables

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "core"
-  private_cluster_enabled = false
+  private_cluster_enabled = var.private_cluster
 
   default_node_pool {
     name       = "corepool"
--- variable.tf
+++ variable.tf
@@ -1,3 +1,7 @@
 variable "environment" {
   type = string
 }
+
+variable "private_cluster" {
+  type = bool
+}
